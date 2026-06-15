5143747b48c4d33bf7aca2a8b5dda9fe938ba2a9
Erik Riberi <erik.riberi.dev@gmail.com>
2026-06-15 17:46:08 -0300

    feat: agregar validación de opciones de menú y rangos para filtros

--- validaciones.py
+++ validaciones.py
@@ -64,3 +64,42 @@ def validar_continente(continente):
     raise ValueError("El continente no puede estar vacío.")
   elif continente not in CONTINENTES:
     raise ValueError("El continente no es válido.\nIngrese África, América, Antártida, Asia, Europa o Oceanía.")
+
+def validar_opcion_menu(opcion, rango):
+  """
+  Valida que la opción del menú no esté vacía y que sea un número entero dentro del rango especificado.
+  
+  Args:
+    opcion (str): La opción a validar.
+    rango (tuple): El rango de valores válidos (min, max).
+  
+  Raises:
+    ValueError: Si la opción está vacía, no es un número entero o está fuera del rango.
+  """
+  if not opcion.strip():
+    raise ValueError("La opción no puede estar vacía.")
+  elif not opcion.isdigit():
+    raise ValueError("La opción debe ser un número entero.")
+  elif int(opcion) < rango[0] or int(opcion) > rango[1]:
+    raise ValueError(f"El valor ingresado debe ser un número entre {rango[0]} y {rango[1]}.")
+
+def validar_rango(numero1, numero2):
+  """
+  Valida que los números no estén vacíos y que sean números enteros mayores o iguales a 0.
+  También valida que el primer número sea menor o igual al segundo.
+  
+  Args:
+    numero1 (str): El primer número a validar.
+    numero2 (str): El segundo número a validar.
+  
+  Raises:
+    ValueError: Si los números están vacíos, no son números enteros o son menores a 0, o si el primer número es mayor al segundo.
+  """
+  if not numero1.strip() or not numero2.strip():
+    raise ValueError("Los números no pueden estar vacíos.")
+  elif not numero1.isdigit() or not numero2.isdigit():
+    raise ValueError("Los valores deben ser números enteros.")
+  elif int(numero1) < 0 or int(numero2) < 0:
+    raise ValueError("Los números no pueden ser menores a 0.")
+  elif int(numero1) > int(numero2):
+    raise ValueError("El primer número debe ser menor o igual al segundo.")
